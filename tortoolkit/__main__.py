c50a67330545048587db441259665fff061b299c
rk125007 <88337232+rk125007@users.noreply.github.com>
2021-08-17 14:15:26 +0530

    Update __main__.py

--- tortoolkit/__main__.py
+++ tortoolkit/__main__.py
@@ -76,6 +76,6 @@ if __name__ == "__main__":
     except:
         pass
 
-    logging.info("THE BOT IS READY TO GOOOOOOO")
+    logging.info("Iam Onlien My Master")
 
     ttkbot.run_until_disconnected()
